ba529d9af4b87016b4224fb3aca9efd2e2585556
Samily <samily.sss2005@gmail.com>
2024-11-26 19:25:01 -0300

    final

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome (str) {
+    let newOne = str.toLowerCase().replaceAll(" ","");
+    let strReverse = "";
+
+    for (let i = newOne.length - 1; i >= 0; i--) {
+        strReverse += newOne[i]
+    }
+    if (newOne === strReverse) {
+        return true
+    }
+    return false;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin (arr) {
+    let max = arr[0];
+    let min = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+        
+    } 
+       return [min, max]
 }
